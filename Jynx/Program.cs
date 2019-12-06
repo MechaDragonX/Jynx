e880cbed5a57088aeef341f5165dfbd3adebd4de
MechaDragonX <sonicraghav@gmail.com>
2019-12-06 08:50:30 -0800

    Add project files.

---
+++ Jynx/Program.cs
@@ -0,0 +1,27 @@
+﻿using DSharpPlus;
+using System;
+using System.Threading.Tasks;
+
+namespace Jynx
+{
+    class Program
+    {
+        static DiscordClient discord;
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+        static async Task MainAsync(string[] args)
+        {
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "<your token here>",
+                TokenType = TokenType.Bot
+            });
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
